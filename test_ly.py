247b0d5d210229e6766ab0edd3de6683d52783cb
Chih Yun <wuchihy@oregonstate.edu>
2021-05-31 18:01:36 -0700

    implemented printing 'It is a leap year' if the year is divisible by 4 and not 100

--- test_ly.py
+++ test_ly.py
@@ -6,4 +6,8 @@ def test_not_divisible_by_4():
 def test__divisible_by_4_and_100():
         value = (2000)
         assert leapYear.leapYear(value) == "It is a leap year"
+def test_divisible_by_4_and_not_100():
+	value = (2040)
+        assert leapYear.leapYear(value) == "It is a leap year"
+
 
